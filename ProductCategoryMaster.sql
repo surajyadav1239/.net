56e4d7a4fdd5791cf08d21de5a65eb47cfedee19
surajyadav1239 <107173636+surajyadav1239@users.noreply.github.com>
2022-06-09 12:39:01 +0530

    Add files via upload

---
+++ ProductCategoryMaster.sql
@@ -0,0 +1,33 @@
+CREATE table dbo.Product
+(
+ProductId int identity,
+ProdutName varchar(100),
+CategoryId int  ,
+CategoryName varchar(100)
+)
+Select * from Product
+
+drop table Product
+
+insert into Product (ProdutName,CategoryId, CategoryName) values (  'Brown Goods' ,1,   'Brown Goods');
+insert into Product (ProdutName,CategoryId, CategoryName)values  (  'Brown Goods' , 2,  'Brown Goods');
+insert into Product (ProdutName,CategoryId, CategoryName)values  (  'Brown Goods' ,  3, 'Brown Goods');
+insert into Product (ProdutName,CategoryId, CategoryName)values  (  'Brown Goods' ,  4, 'Brown Goods');
+insert into Product (ProdutName,CategoryId, CategoryName)values  (  'Brown Goods' ,   5,'Brown Goods');
+insert into Product (ProdutName,CategoryId, CategoryName)values  (  'Brown Goods' ,   6,'Brown Goods');
+insert into Product (ProdutName,CategoryId, CategoryName)values  (  'Brown Goods' ,   7,'Brown Goods');
+insert into Product (ProdutName,CategoryId, CategoryName)values  (  'Brown Goods' ,   8,'Brown Goods');
+insert into Product (ProdutName,CategoryId, CategoryName)values  (  'Brown Goods' ,   9,'Brown Goods');
+insert into Product (ProdutName,CategoryId, CategoryName)values  (  'Brown Goods' ,  10, 'Brown Goods');
+insert into Product (ProdutName,CategoryId, CategoryName)values  (  'Brown Goods' , 11,  'Brown Goods');
+insert into Product (ProdutName,CategoryId, CategoryName)values  (  'Brown Goods' ,  12, 'Brown Goods');
+insert into Product (ProdutName,CategoryId, CategoryName)values  (  'Brown Goods' ,  13, 'Brown Goods');
+insert into Product (ProdutName,CategoryId, CategoryName)values  (  'Brown Goods' ,  14, 'Brown Goods');
+insert into Product (ProdutName,CategoryId, CategoryName)values  (  'Brown Goods' ,  15, 'Brown Goods');
+insert into Product (ProdutName,CategoryId, CategoryName)values  (  'Brown Goods' ,  16, 'Brown Goods');
+insert into Product (ProdutName,CategoryId, CategoryName)values  (  'Brown Goods' ,  17, 'Brown Goods');
+insert into Product (ProdutName,CategoryId, CategoryName)values  (  'Brown Goods' , 18 , 'Brown Goods');
+insert into Product (ProdutName,CategoryId, CategoryName)values  (  'Brown Goods' , 19 , 'Brown Goods');
+insert into Product (ProdutName,CategoryId, CategoryName)values  (  'Brown Goods' , 20  ,'Brown Goods');
+insert into Product (ProdutName,CategoryId, CategoryName)values  (  'Brown Goods' ,  21 ,'Brown Goods');
+insert into Product (ProdutName,CategoryId, CategoryName)values  (  'Brown Goods' ,  22, 'Brown Goods');
